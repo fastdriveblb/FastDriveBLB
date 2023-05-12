c138615adba66b5ad32c25cb472e738c9e0708f0
fastdriveblb <133349309+fastdriveblb@users.noreply.github.com>
2023-05-12 12:27:31 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-window.addEventListener('load', () => {
+document.addEventListener('DOMContentLoaded', () => {
     const container = document.querySelector('.container');
     container.classList.add('animate__animated', 'animate__fadeInUp');
 
