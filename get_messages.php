d2d2511a0dfde9120acc359315b3f50d375aef4a
fastdriveblb <133349309+fastdriveblb@users.noreply.github.com>
2023-05-14 21:17:09 +0200

    Delete get_messages.php

--- get_messages.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Retrieve the messages from the file or database
-// For simplicity, we'll read them from a text file
-$messages = file('messages.txt', FILE_IGNORE_NEW_LINES);
-
-echo json_encode($messages);
-?>
